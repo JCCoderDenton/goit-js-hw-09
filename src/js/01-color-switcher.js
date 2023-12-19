7e52084d45819aceea820dad0182652c0e1faa0e
JCCoderDenton <denys_m99@ukr.net>
2023-12-19 20:00:40 +0200

    Initial commit

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,20 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let intervalId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+function startColorSwitching () {
+    intervalId = setInterval(function () {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+};
+
+function stopColorSwitching () {
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+};
+startBtn.addEventListener('click', startColorSwitching);
+stopBtn.addEventListener('click', stopColorSwitching);
