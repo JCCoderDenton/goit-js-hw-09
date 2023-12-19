607c882e2210165a3ef582d23221d33d0f71d8db
JCCoderDenton <98703252+JCCoderDenton@users.noreply.github.com>
2023-12-19 18:13:00 +0000

    Deploying to gh-pages from @ JCCoderDenton/goit-js-hw-09@5ddeee7daa2a679d04e24c714b0c314fd4032eb9 🚀

---
+++ 01-color-switcher.a97c1734.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=null;t.addEventListener("click",(function(){e=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){clearInterval(e),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.a97c1734.js.map
